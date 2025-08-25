d220ee81ae37a0cf61ecd0c9f1ffa2e3d7b6b134
Satyam Tyagi <satyamtyagiji15@gmail.com>
2025-08-26 00:33:11 +0530

    Add files via upload

---
+++ 2.Data types in js/script.js
@@ -0,0 +1,16 @@
+let a = 123
+console.log("my roll no is ", a);
+console.log("type of a :",typeof(a));
+let b = 'satyam tyagi'
+console.log("my name is ", b);
+console.log("type of a :",typeof(b));
+// we can also change the data type for eg
+let abc = 123;
+console.log('type of abc ',typeof(abc));
+abc = String(abc);
+console.log('type of abc',typeof(abc));
+// if we convert a no into boolean we'll always get true why is that so it's because except 0 all the no are considered as the true in boolean.for eg
+let abcd = 123;
+console.log('type of abcd',typeof(abcd),abcd);
+abcd = Boolean(abcd)
+console.log('type of abcd',typeof(abcd),abcd);
