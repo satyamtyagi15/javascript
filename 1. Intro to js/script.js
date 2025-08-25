40e01b102e31868f305f8952e214db7a442e8fb5
Satyam Tyagi <satyamtyagiji15@gmail.com>
2025-08-26 00:30:21 +0530

    Add files via upload

---
+++ 1. Intro to js/script.js
@@ -0,0 +1,18 @@
+        let name = "Satyam Tyagi" ;
+        let age = 20
+        let a = 20;
+        let b = 30;
+        let sum = a+b;
+        //now in order to print this thing we can use console.log for the following task
+        console.log("My name is :",name);
+        console.log("My age is : ", age);
+        console.log("the addition = :",sum);
+        console.log("the subtraction =:",sum - 5 );
+        // we use let keyword for variable because it's mutable for eg;
+        let varname= "satyam";
+        console.log(varname);
+         varname = "shivam";
+        console.log(varname);
+        // NOTE -> here if we see the output it's gonna print 1)satyam 2)shivam so here it can overwrite values where as const keeps the value first assigned as the constant value for that variable.
+        // NOTE -> now the another keyword var is same is let they both are mutable the only difference between the two is that let is a local variable whereas var is a global variable.
+        // js is case sensitive
